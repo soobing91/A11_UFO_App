921dca45004f6cb77486da882ea12a4bdbdf84ca
Soo Bin Hwang <chaotic521@hotmail.com>
2019-09-08 12:59:50 -0400

    Part 1 completed

--- static/js/app.js
+++ static/js/app.js
@@ -1,4 +1,46 @@
 // from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
+// Create a table
+// Capture tbody
+var tbody = d3.select('tbody');
+tbody.html('');
+
+// Construct tbody
+tableData.forEach((sighting) => {
+    var row = tbody.append('tr');
+    Object.entries(sighting).forEach(([key, value]) => {
+        var cell = row.append('td');
+        cell.text(value);
+    });
+});
+
+// Listen events
+var button = d3.select('#filter-btn');
+
+button.on('click', function() {
+    
+    // Capture input element
+    var inputElement = d3.select('#datetime');
+    var inputValue = inputElement.property('value');    
+    console.log(inputValue);
+
+    // Filter the table
+    var filteredData = tableData.filter((sighting) => sighting.datetime === inputValue);
+    console.log(filteredData);
+
+    // Create the filtered table
+    // Reset tbody
+    tbody.html('');
+
+    // Reconstruct tbody
+    filteredData.forEach((sighting) => {
+        var row = tbody.append('tr');
+        Object.entries(sighting).forEach(([key, value]) => {
+            var cell = row.append('td');
+            cell.text(value);
+        });
+    });
+});
+
+
